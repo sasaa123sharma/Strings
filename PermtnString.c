4d354bea39f20846f8dadfe39f851e12ab0cdf94
sasaa sharma <sasaa123sharma@gmail.com>
2016-08-16 15:01:49 +0530

    Create PermtnString.c

---
+++ PermtnString.c
@@ -0,0 +1,55 @@
+//Print all permutations of String(includes duplicates)
+//Test cases
+//1. NULL string
+//2. Non NULL string
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void swap(char *x, char *y)
+{
+	char ch = *x;
+	*x = *y;
+	*y = ch;
+	return ;
+}
+
+void PermtnString(char *str, int start, int end, int *count)
+{
+	if(start == end)//nothing more left to permute 
+	{
+		(*count)++;	
+		printf(" %d - %s\n", *count, str);
+	}
+	else
+	{	
+		int i = start;
+		
+		for(; i <= end	; i++)
+		{
+			swap(&str[start], &str[i]); 
+			PermtnString(str, start+1, end, count);
+			//swap again
+			swap(&str[start], &str[i]); //backtrack step	
+		} 
+	}
+
+	return ;	
+}
+
+int main()
+{
+	char str[50] = {0};
+	int count = 0;
+
+	printf("enter the string\n");
+	
+	gets(str);
+
+	printf("Permutations of string - \n");
+
+	PermtnString(str, 0, strlen(str)- 1, &count);
+
+	return 0;
+}
